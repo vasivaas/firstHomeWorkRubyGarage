2ed87ad7e296f4f40ab616dc498c40b108370867
Vasyl <wkichira@gmail.com>
2020-05-24 14:31:52 +0300

    add triangle method in file triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  arr = [a, b, c].sort
+  firstSec = arr[0] + arr[1]
+  third = arr[2]
+  raise TriangleError if firstSec <= third
+  arr = [a, b, c].uniq
+  case arr.size
+  when 1
+    :equilateral
+  when 2
+    :isosceles
+  when 3
+    :scalene
+  else
+    :wow
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
