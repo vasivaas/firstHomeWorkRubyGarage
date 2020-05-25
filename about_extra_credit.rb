2768373033073ecfbc36b7701931346a414fe9a8
Vasyl <wkichira@gmail.com>
2020-05-25 15:49:28 +0300

    created class Player in file about_extra_credit.rb

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -6,3 +6,29 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+require '../koans/about_dice_project'
+require '../koans/about_scoring_project'
+
+
+class Player
+  attr_reader :player_name, :score, :dices
+
+  def initialize(player_name, score = 0)
+    @player_name = player_name
+    @score = score
+    @dices = DiceSet.new
+  end
+
+  def roll(n)
+    @dices.roll(n)
+  end
+
+  def to_count_score(score)
+    @score += score
+  end
+end
+
+class Game
+  
+end
